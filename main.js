4c6f4b01bbae18b6531f5b364e613bd58fd7e215
umarxoja <107460384+umarxoja@users.noreply.github.com>
2022-06-25 12:19:06 +0500

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-
-var quotes = ["Aziz","Said" ,"Asad" ,"Umar" ,"Aziz1","Said1" ,"Asad1" ,"Umar1" ,"Aziz2","Said2" ,"Asad2" ,"Umar2"];
-
-function genQuote() {
-  var randNum = Math.floor(Math.random() * quotes.length);
-  document.getElementById('quote').innerHTML = quotes[randNum];
-}
